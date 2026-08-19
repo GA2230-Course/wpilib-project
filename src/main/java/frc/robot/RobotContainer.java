ba3b622d38fd6579acb5c5bb401970b765cc6f9a
daphnachen-ctrl <daphna.chen@gmail.com>
2026-08-19 15:54:09 +0300

    moved timed intake from robot to robot container

--- src/main/java/frc/robot/RobotContainer.java
+++ src/main/java/frc/robot/RobotContainer.java
@@ -5,16 +5,25 @@
 package frc.robot;
 
 import edu.wpi.first.wpilibj2.command.Command;
-import edu.wpi.first.wpilibj2.command.Commands;
+
+import edu.wpi.first.wpilibj2.command.button.CommandXboxController;
+import frc.robot.subsystems.Intake;
+import frc.robot.commands.OpenIntakeWithTimeout;
 
 public class RobotContainer {
+
+  private final Intake m_intake = new Intake();
+  private final CommandXboxController m_controller = new CommandXboxController(0);
+
   public RobotContainer() {
     configureBindings();
   }
 
-  private void configureBindings() {}
+  private void configureBindings() {
+    m_controller.a().onTrue(new OpenIntakeWithTimeout(m_intake, 3));
+  }
 
   public Command getAutonomousCommand() {
-    return Commands.print("No autonomous command configured");
+    return new OpenIntakeWithTimeout(m_intake, 3.0);
   }
 }

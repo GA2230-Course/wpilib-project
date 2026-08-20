6ecd1d1263d3434cb1e803ecb3e9be940bad71b7
daphnachen-ctrl <daphna.chen@gmail.com>
2026-08-20 13:49:52 +0300

    fixed RobotContainer

--- src/main/java/frc/robot/RobotContainer.java
+++ src/main/java/frc/robot/RobotContainer.java
@@ -7,12 +7,17 @@ package frc.robot;
 import edu.wpi.first.wpilibj2.command.Command;
 
 import edu.wpi.first.wpilibj2.command.button.CommandXboxController;
+import frc.robot.subsystems.Elevator;
 import frc.robot.subsystems.Intake;
+
 import frc.robot.commands.OpenIntakeWithTimeout;
+import frc.robot.commands.MoveElevatorWithTimeout;
+import frc.robot.commands.TakeAndElevate; 
 
 public class RobotContainer {
 
   private final Intake m_intake = new Intake();
+  private final Elevator m_elevator = new Elevator();
   private final CommandXboxController m_controller = new CommandXboxController(0);
 
   public RobotContainer() {
@@ -21,9 +26,11 @@ public class RobotContainer {
 
   private void configureBindings() {
     m_controller.a().onTrue(new OpenIntakeWithTimeout(m_intake, 3));
+    m_controller.b().onTrue(new MoveElevatorWithTimeout(m_elevator, 1, 3));
+    m_controller.x().onTrue(new TakeAndElevate(m_intake, m_elevator, 3, 30));
   }
 
   public Command getAutonomousCommand() {
-    return new OpenIntakeWithTimeout(m_intake, 3.0);
+    return new TakeAndElevate(m_intake, m_elevator, 3, 10);
   }
 }

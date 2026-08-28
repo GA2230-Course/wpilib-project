1a6922c10c03f94dd7a9b0e67db137149adb9679
daphnachen-ctrl <daphna.chen@gmail.com>
2026-08-28 10:53:08 +0300

    made superstructure to use the command states

--- src/main/java/frc/robot/subsystems/SuperstructureSubsystem.java
+++ src/main/java/frc/robot/subsystems/SuperstructureSubsystem.java
@@ -1,41 +1,104 @@
 package frc.robot.subsystems;
 
+import edu.wpi.first.wpilibj.Timer;
+
 public class SuperstructureSubsystem extends StateMachineSubsystem<SuperstructureSubsystem.SuperState> {
 
     public enum SuperState {
         IDLE,
-        INTAKE,
-        ELEVATOR_UP
+        OPEN_INTAKE_TIMED,
+        MOVE_ELEVATOR_TIMED,
+        TAKE_AND_ELEVATE_INTAKE_STAGE,
+        TAKE_AND_ELEVATE_LIFT_STAGE
     }
 
     private Intake intake;
     private Elevator elevator;
+    private final Timer timer = new Timer();
 
-    public SuperstructureSubsystem() {
+    private static final double INTAKE_TIMEOUT = 2.0;
+    private static final double ELEVATOR_MOVE_TIMEOUT = 1.5;
+    private static final double ELEVATOR_SPEED_STEP = 5.0;
+    private static final double ELEVATOR_START_HEIGHT = 0.0;
+    private static final double TARGET_ELEVATOR_HEIGHT = 500.0;
+
+    public SuperstructureSubsystem(Intake intake, Elevator elevator) {
         super(SuperState.IDLE);
-        this.intake = new Intake();
-        this.elevator = new Elevator();
+        this.intake = intake;
+        this.elevator = elevator;
+    }
+
+    @Override
+    public void setWantedState(SuperState wantedState) {
+        if (wantedState != getWantedState()) {
+            timer.reset();
+            timer.start();
+        }
+        super.setWantedState(wantedState);
     }
 
     @Override
     protected SuperState handleStateTransition() {
-        return getWantedState();
+        SuperState wanted = getWantedState();
+
+        switch (wanted) {
+            case OPEN_INTAKE_TIMED:
+                if (timer.hasElapsed(INTAKE_TIMEOUT)) {
+                    timer.stop();
+                    setWantedState(SuperState.IDLE);
+                    return SuperState.IDLE;
+                }
+                return SuperState.OPEN_INTAKE_TIMED;
+
+            case MOVE_ELEVATOR_TIMED:
+                if (timer.hasElapsed(ELEVATOR_MOVE_TIMEOUT)) {
+                    timer.stop();
+                    setWantedState(SuperState.IDLE);
+                    return SuperState.IDLE;
+                }
+                return SuperState.MOVE_ELEVATOR_TIMED;
+
+            case TAKE_AND_ELEVATE_INTAKE_STAGE:
+                if (timer.hasElapsed(INTAKE_TIMEOUT)) {
+                    timer.stop();
+                    setWantedState(SuperState.TAKE_AND_ELEVATE_LIFT_STAGE);
+                    return SuperState.TAKE_AND_ELEVATE_LIFT_STAGE;
+                }
+                return SuperState.TAKE_AND_ELEVATE_INTAKE_STAGE;
+
+            case TAKE_AND_ELEVATE_LIFT_STAGE:
+            case IDLE:
+            default:
+                return wanted;
+        }
     }
 
     @Override
     protected void applyCurrentState() {
         switch (getCurrentState()) {
-            case INTAKE:
-                this.elevator.setHeight(0);
-                this.intake.open();
+            case OPEN_INTAKE_TIMED:
+                elevator.setHeight(ELEVATOR_START_HEIGHT);
+                intake.open();
                 break;
-            case ELEVATOR_UP:
-                this.intake.close();
-                this.elevator.setHeight(500);;
+
+            case MOVE_ELEVATOR_TIMED:
+                intake.close();
+                elevator.setHeight(elevator.getHeight() + ELEVATOR_SPEED_STEP);
                 break;
+
+            case TAKE_AND_ELEVATE_INTAKE_STAGE:
+                elevator.setHeight(0);
+                intake.open();
+                break;
+
+            case TAKE_AND_ELEVATE_LIFT_STAGE:
+                intake.close();
+                elevator.setHeight(TARGET_ELEVATOR_HEIGHT);
+                break;
+
             case IDLE:
-                this.intake.close();
-                this.elevator.setHeight(0);
+            default:
+                intake.close();
                 break;
         }
     }
